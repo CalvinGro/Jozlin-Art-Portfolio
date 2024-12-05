1c82a87e32506d588d60f047c1bbfc6247a6510c
Calvin Gross <chiriquiglassfrog@icloud.com>
2024-12-05 08:59:48 -0600

    Initial Commit

---
+++ website/views.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, render_template
+
+views = Blueprint("views", __name__)
+
+@views.route('/')
+def home():
+    return render_template("home.html")
+
+@views.route('/gallery')
+def gallery():
+    return render_template("gallery.html")
+
+@views.route('/aboutme')
+def about_me():
+    return render_template("aboutme.html")
+
+@views.route('/contact')
+def contact():
+    return render_template("contact.html")
