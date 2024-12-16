60b6b9ef59919442f665ae7941846c2dec9a2274
Calvin Gross <chiriquiglassfrog@icloud.com>
2024-12-16 17:23:38 -0600

    updated portfolio and aboutme page

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from website import create_app
 
+
+
+
+
 app = create_app()
+
+# app.config['SECRET_KEY'] = 'wreoigerwoigiufdpoifjfdewrpoijro cxmvewfiwoiefpo'
+# app.config['Mail_SERVER'] = 'smtp.gmail.com'
+# app.config['Mail_PORT'] = 465
+# app.config['MAIL_USERNAME'] = 'calvingwrestle@gmail.com'
+# app.config['MAIL_PASSWORD'] = 'SweetSawfi5h'
+# app.config['MAIL_USE_TLS'] = False
+# app.config['MAIL_USE_SSL'] = True
 
 if __name__ == '__main__':
     app.run(debug=True)
